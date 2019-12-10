9d2c78cb753259ab694e906b791eb4454daee9c6
Ahmad Faizuddin <ahmadfaizuddin17@gmail.com>
2019-12-10 20:47:31 +0900

    Delete registration token confirmation

--- home/views.py
+++ home/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib.sites.shortcuts import get_current_site
-from django.contrib.auth import login, authenticate
-from django.utils.encoding import force_bytes, force_text
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.template.loader import render_to_string
-from .token_generator import account_activation_token
-from django.core.mail import EmailMessage
+# from django.contrib.sites.shortcuts import get_current_site
+# from django.contrib.auth import login, authenticate
+# from django.utils.encoding import force_bytes, force_text
+# from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+# from django.template.loader import render_to_string
+# from .token_generator import account_activation_token
+# from django.core.mail import EmailMessage
 
 from .forms import UserRegistrationForm, UpdateDetailForm, UpdateProfileForm, UpdateUserForm
 from .models import List
@@ -97,43 +97,16 @@ def register(request):
     if request.method == "POST":
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
             email = form.cleaned_data["email"]
             if not User.objects.filter(email=email).exists():
-                user.is_active = False
-                user.save()
-                current_site = get_current_site(request)
-                email_subject = "Activate Your Account"
-                message = render_to_string('activate_account.html', {
-                    'user': user,
-                    'domain': current_site.domain,
-                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                    'token': account_activation_token.make_token(user),
-                })
-                email = EmailMessage(email_subject, message, to=[email])
-                email.send()
-                return HttpResponse("We have sent you an email,please confirm your email adress")
+                form.save()
+                return redirect("login")
             else:
                 messages.info(request, "The email is already in used")
                 return render(request, "register.html", {"form": form})
     else:
         form = UserRegistrationForm
     return render(request, "register.html", {"form": form})
-
-
-def activate(request, uidb64, token):
-    try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
-    if user is not None and account_activation_token.check_token(user, token):
-        user.is_active = True
-        user.save()
-        login(request, user)
-        return redirect("login")
-    else:
-        return HttpResponse('invalid token')
 
 
 # <==========================================================================================>
